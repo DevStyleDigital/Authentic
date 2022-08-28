c3df6a62f7b4c2dbe5f29828c12fc160fb80be61
Afonso <afonsobezerra199@gmail.com>
2022-08-28 17:04:27 -0300

    build: first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { LayoutProvider } from '@contexts/Layout';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <title>Instituto Humberto de Campos</title>
+      </Head>
+      <LayoutProvider>
+        <Component {...pageProps} />
+      </LayoutProvider>
+    </>
+  );
+};
+
+export default MyApp;
