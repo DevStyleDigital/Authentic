6b7afc8c8fb14f3ca71817f57409060a1c82d470
Afonso Bezerra <afonsobezerra199@gmail.com>
2023-05-02 19:25:38 -0300

    tag google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-
+import Script from 'next/script';
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
@@ -43,10 +43,30 @@ export default class MyDocument extends Document {
           {/* METAS */}
           <meta charSet="utf-8" />
           <meta name="theme-color" content="#000000" />
+          {/* Google Tag Manager */}
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P3SMFHC');
+            `}
+          </Script>
+          {/* End Google Tag Manager */}
         </Head>
         <body>
           <Main />
           <NextScript />
+          {/* Google Tag Manager (noscript) */}
+                    <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P3SMFHC"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+                `,
+            }}
+          />
+          {/* End Google Tag Manager (noscript) */}
         </body>
       </Html>
     );
