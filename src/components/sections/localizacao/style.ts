1aee758b6e6b143e44559410dcacc0623a9d4aca
Afonso Bezerra <afonsobezerra199@gmail.com>
2023-05-05 10:05:11 -0300

    fix heigth

--- src/components/sections/localizacao/style.ts
+++ src/components/sections/localizacao/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const LocalizacaoStyle = styled.section`
   width: 100%;
-  height: 100vh;
+  height: 80vh;
   img {
     width: 100%;
     height: 100%;
