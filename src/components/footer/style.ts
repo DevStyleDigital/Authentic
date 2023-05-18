0b68d3b730eb142b12fdcf71ca5d115279c27d18
Afonso Bezerra <afonsobezerra199@gmail.com>
2023-05-18 17:44:17 -0300

    fix seo

--- src/components/footer/style.ts
+++ src/components/footer/style.ts
@@ -2,32 +2,44 @@ import styled from 'styled-components';
 
 export const FooterStyle = styled.section`
   width: 100%;
-  height: auto;
+  height: fit-content;
   min-height: 20vh;
-  background-image: url(/texture-footer.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 2rem 3rem;
-  gap: 1rem;
-  span {
-    text-align: center;
-    font-size: 13px;
+  position: relative;
+  img{
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    position: absolute;
+    z-index: 0;
   }
-  svg {
-    width: 200px;
-  }
-  .logo {
+  .content {
+    position: relative;
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
-    gap: 0.5rem;
     align-items: center;
-    a {
-      color: black;
-      text-decoration: none;
-      font-weight: bold;
+    justify-content: center;
+    gap: 1rem;
+    padding: 2rem 3rem; 
+    span {
+      text-align: center;
+      font-size: 13px;
+    }
+    svg {
+      width: 200px;
+    }
+    .logo {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+      align-items: center;
+      a {
+        color: black;
+        text-decoration: none;
+        font-weight: bold;
+      }
     }
   }
 `;
